8dda5cd37f4c22b794f883ad8c54a150cd6be9c2
Amir Pourmand <pourmand1376@gmail.com>
2022-06-15 13:06:16 +0430

    add changes

--- print_dcm_header.py
+++ print_dcm_header.py
@@ -1,44 +1,39 @@
 from argparse import ArgumentParser
-from os import path, makedirs, sep
+from os import makedirs, path
+from pathlib import Path
+
 import pydicom
-import types
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('-i', nargs='+', dest='input_files', required=True)
-    parser.add_argument('-o', dest='save_dir', default=None)
+    parser.add_argument("-i", nargs="+", dest="input_files", required=True)
+    parser.add_argument("-o", dest="save_dir", default=None)
+    parser.add_argument(
+        "--save_in_parent", dest="save_in_parent", required=False, default=False
+    )
     args = parser.parse_args()
+
+    if args.save_in_parent:
+        path = Path(args.input_files[0])
+        args.save_dir = path.absolute() / "header_data"
 
     if args.save_dir is not None:
         makedirs(args.save_dir, exist_ok=True)
 
     for dcm_f in args.input_files:
         try:
+            dcm_f = Path(dcm_f)
             dcm = pydicom.dcmread(dcm_f, stop_before_pixels=True)
-            #dcm = pydicom.dcmread(dcm_f)
 
             if args.save_dir is not None:
-                sd = path.join(args.save_dir, dcm_f)
-                makedirs(path.dirname(sd), exist_ok=True)
-                out = open(sd, 'w')
+                sd = args.save_dir / Path(dcm_f.name + "_hdr.txt")
+                out = open(sd, "w")
             else:
-                out = open(dcm_f + '_hdr.txt', 'w')
+                out = open(str(dcm_f) + "_hdr.txt", "w")
 
-            for f in dir(dcm):
-
-                # if f.startswith('_'):
-                #    continue
-
-                if 'pixel' not in f.lower():
-
-                    val = getattr(dcm, f)
-
-                    # if isinstance(val, (types.FunctionType, types.BuiltinFunctionType, types.MethodType, types.BuiltinMethodType)):
-                    #    continue
-
-                    out.write(f'****\n{f}\n@\n{val}\n\n')
-
+            out.write(str(dcm))
             out.close()
-        except:
-            continue
+            print(f"File {dcm_f} successfully processed.")
+
+        except Exception:
+            pass
--- print_dcm_img.py
+++ print_dcm_img.py
@@ -1,16 +1,16 @@
 from argparse import ArgumentParser
-from os import path, makedirs, sep, walk
-import pydicom
-import types
 from multiprocessing import Pool
-import numpy as np
-import imageio
+from os import makedirs, path, sep, walk
+
 import cv2
+import imageio
+import numpy as np
 import pandas as pd
+import pydicom
 
 
 def _correct_image_color_space(dcm) -> np.ndarray:
-    """ Corrects the image's color space and returns the numpy array
+    """Corrects the image's color space and returns the numpy array
 
     Args:
         dcm (FileDataset): The dicom filedataset
@@ -21,11 +21,11 @@ def _correct_image_color_space(dcm) -> np.ndarray:
 
     img = dcm.pixel_array.astype(np.int64)
 
-    pmi = dcm.get('PhotometricInterpretation', None)
-    wc = dcm.get('WindowCenter', None)
-    ww = dcm.get('WindowWidth', None)
-    rs = dcm.get('RescaleSlope', None)
-    ri = dcm.get('RescaleIntercept', None)
+    pmi = dcm.get("PhotometricInterpretation", None)
+    wc = dcm.get("WindowCenter", None)
+    ww = dcm.get("WindowWidth", None)
+    rs = dcm.get("RescaleSlope", None)
+    ri = dcm.get("RescaleIntercept", None)
 
     # Transforming the values linearly
 
@@ -36,7 +36,7 @@ def _correct_image_color_space(dcm) -> np.ndarray:
         img = img + ri
 
     # Inverting the white-black images to black-white
-    
+
     x = img.astype(np.float64)
 
     if (ww is not None) and (wc is not None):
@@ -59,48 +59,50 @@ def _correct_image_color_space(dcm) -> np.ndarray:
             y = x
 
     y = np.round(y).astype(np.uint16)
-    
+
     return y
 
 
 def check_file(dcm_path, dcm_pref, save_dir):
 
     try:
-                
         dcm = pydicom.read_file(dcm_path)
         img = _correct_image_color_space(dcm)
-        sd = path.join(save_dir, dcm_path.replace(dcm_pref, '').replace('..' + sep, '').replace(sep, '@'))
+        sd = path.join(
+            save_dir,
+            dcm_path.replace(dcm_pref, "").replace(".." + sep, "").replace(sep, "@"),
+        )
         makedirs(path.dirname(sd), exist_ok=True)
-        imageio.imwrite(sd + '.png', cv2.resize(img, (512, 512)).astype(np.uint16))
+        imageio.imwrite(sd + ".png", cv2.resize(img, (512, 512)).astype(np.uint16))
 
-    except:
-        print(f'Problem in {dcm_path}')
-    
+    except Exception:
+        print(f"Problem in {dcm_path}")
+
     return False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = ArgumentParser()
-    parser.add_argument('data_root')
-    parser.add_argument('save_dir')
-    parser.add_argument('-p', dest='pref', default='')
-    parser.add_argument('-c', dest='cores', type=int, default=32)
+    parser.add_argument("data_root")
+    parser.add_argument("save_dir")
+    parser.add_argument("-p", dest="pref", default="")
+    parser.add_argument("-c", dest="cores", type=int, default=32)
     args = parser.parse_args()
 
     if path.isfile(args.data_root):
-        info = pd.read_csv(args.data_root, sep=',', header=0)
-        dcm_paths = info['dcm_path'].values
+        info = pd.read_csv(args.data_root, sep=",", header=0)
+        dcm_paths = info["dcm_path"].values
     else:
         dcm_paths = []
         for r, _, files in walk(args.data_root):
             for f in files:
                 dcm_paths.append(path.join(r, f))
 
-    print(f'Found {len(dcm_paths)} dcm files.')
+    print(f"Found {len(dcm_paths)} dcm files.")
 
     pool = Pool(args.cores)
     cnts = pool.starmap(check_file, [(x, args.pref, args.save_dir) for x in dcm_paths])
     pool.close()
 
-    print(f'Found {sum(cnts)} spots.')
+    print(f"Found {sum(cnts)} spots.")
