6c983fe35d8b7a3e6f0c3e3f4fc4a821422495c6
Amir Pourmand <pourmand1376@gmail.com>
2022-11-28 11:53:49 +0330

    add download.sh

--- PolypSets/download.sh
+++ PolypSets/download.sh
@@ -1,3 +1,4 @@
+# https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/FCBUOR
 wget https://dataverse.harvard.edu/api/access/datafile/4638276
 mv 4638276 Polypsets.zip
 unzip Polypsets.zip
